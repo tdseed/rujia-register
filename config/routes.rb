f4c178b93de00729474af7d2b56b5d1779e333bf
crayon <xiangxin.wu@gmail.com>
2014-06-26 15:46:30 +0800

    去掉路由前缀

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
-	scope :qrcode do
-	  root to: "users#new"
-	  
-	  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-	  
-	  get 'signup', to: 'users#new', as: 'signup'
+  root to: "users#new"
+  
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  
+  get 'signup', to: 'users#new', as: 'signup'
 
-	  resources :users, only: [:create] do
-	    collection do
-	      get :check_phone
-	    end
-	  end
-	end
-
+  resources :users, only: [:create] do
+    collection do
+      get :check_phone
+    end
+  end
+  
 end
