4d31cd8e0105d22560250d614df918060260c109
tommy <tommy@bestapp.us>
2014-07-07 18:27:09 +0800

    add banner image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,32 +17,24 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new params.require(:user).permit(:name, :phone)
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to :action => "success" }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors }
-      end
-    end
-
-    # @user = User.new
-    # @user.name = params[:name]
-    # @user.phone = params[:phone]
-    # if @user.save
-    #   redirect_to '/over'
-    # else
-    #   render :new
+    # @user = User.new params.require(:user).permit(:name, :phone)
+    # respond_to do |format|
+    #   if @user.save
+    #     format.html { redirect_to :action => "success" }
+    #     format.json { render json: @user, status: :created, location: @user }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @user.errors }
+    #   end
     # end
 
-    # @user = User.new params.require(:user).permit(:name, :phone)
-    #   if @user.save
-    #     redirect_to :action => "success"
-    #   else
-    #     render :new
-    #   end
+    @user = User.new params.require(:user).permit(:name, :phone)
+    if @user.save
+      redirect_to :action => "success"
+    else
+      render :new
+    end
+
   end
 
   def check_phone
